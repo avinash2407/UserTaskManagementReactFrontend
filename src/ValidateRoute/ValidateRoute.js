b594b5e7cdc553ca35612882f4fce75b60d5a3b7
hatecoder <nimmagaddaavinash1998@gmail.com>
2019-07-29 17:29:58 +0530

    Updated Route Validation

--- src/ValidateRoute/ValidateRoute.js
+++ src/ValidateRoute/ValidateRoute.js
@@ -6,7 +6,7 @@ export const ValidateRoute = ({ component: Component, ...rest }) => (
 	<Route
 		{...rest}
 		render={props =>
-			(Cookies.get('tokencookie')!==undefined) ? (
+			((Cookies.get('tokencookie')!==undefined) && (Cookies.get('role')!==undefined) && (Cookies.get('userid')!==undefined)) ? (
 				<Component {...props} />
 			) : (
 				<Redirect to={{ pathname: '/login', state: { from: props.location } }} />
